f87b5a0fadb6833d0e0e16d04078f9bf9e858fd4
J Cole Morrison <j.cole.morrison@gmail.com>
2014-08-21 14:06:09 -0700

    step 13

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  root 'application#index'
+
+  get "*path" => "application#index"
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
