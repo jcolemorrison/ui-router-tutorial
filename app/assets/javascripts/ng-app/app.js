0f6fc04fc2e862a50d2b16c7ae8a2979e6dab252
J Cole Morrison <j.cole.morrison@gmail.com>
2014-08-21 23:34:57 -0700

    end of step20

--- app/assets/javascripts/ng-app/app.js
+++ app/assets/javascripts/ng-app/app.js
@@ -15,7 +15,29 @@ angular
 				url: '/',
 				templateUrl: 'home.html',
 				controller: 'HomeCtrl'
-			});
+			})
+			// an abstract state that just serves as a
+			// parent for the below child states
+			.state('dashboard', {
+				abstract: true,
+				url: '/dashboard',
+				templateUrl: 'dashboard/layout.html'
+			})
+				// the default route when someone hits dashboard
+				.state('dashboard.one', {
+					url: '',
+					templateUrl: 'dashboard/one.html'
+				})
+				// this is /dashboard/two
+				.state('dashboard.two', {
+					url: '/two',
+					templateUrl: 'dashboard/two.html'
+				})
+				// this is /dashboard/three
+				.state('dashboard.three', {
+					url: '/three',
+					templateUrl: 'dashboard/three.html'
+				});
 
 		// default fallback route
 		$urlRouterProvider.otherwise('/');
