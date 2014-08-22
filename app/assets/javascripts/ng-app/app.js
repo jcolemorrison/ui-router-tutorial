ff9f99303dae10cacfd9f3a90a337811839cb5a2
J Cole Morrison <j.cole.morrison@gmail.com>
2014-08-21 20:47:20 -0700

    step16 complete

--- app/assets/javascripts/ng-app/app.js
+++ app/assets/javascripts/ng-app/app.js
@@ -6,4 +6,21 @@ angular
 		'templates'
 	])
 	.config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+
+		/**
+		 * Route and States
+		 */
+		$stateProvider
+			.state('home', {
+				url: '/',
+				templateUrl: 'home.html',
+				controller: 'HomeCtrl'
+			});
+
+		// default fallback route
+		$urlRouterProvider.otherwise('/');
+
+		// enable HTML5 mode for SEO
+		$locationProvider.html5Mode(true);
+
 	});
